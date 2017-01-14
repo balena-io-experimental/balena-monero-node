dfbafdfc9e9c2060f017b93c51fff3dce50b212b
Gergely Imreh <imrehg@gmail.com>
2017-01-13 20:19:48 -0500

    update startscript

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Starting the monero node
-monerod
-  --config-file /etc/monero.conf \
-  --detach
+monerod --config-file /etc/monero.conf --detach
 
-tail -f /root/.bitmonero/monero.log
+while : ; do echo "Running..."; sleep 600; done
