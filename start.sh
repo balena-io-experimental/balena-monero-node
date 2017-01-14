ac5a7c9ba088b8f442de2302c3506cdda20ba931
Gergely Imreh <imrehg@gmail.com>
2017-01-13 20:35:43 -0500

    display monero cpu use

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # Starting the monero node
 monerod --config-file /etc/monero.conf --detach
 
-while : ; do ls -lah /data/monero/lmdb/data.mdb; sleep 600; done
+while : ; do ls -lah /data/monero/lmdb/data.mdb; ps uax | grep "monero"; sleep 600; done
